8a84100aee85da06218e8294b2f1018993b08598
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-08 11:24:48 +0100

    Update ws.php

--- includes/ws.php
+++ includes/ws.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Copyright (C) 2013-2016 xtr4nge [_AT_] gmail.com
+    Copyright (C) 2013-2020 xtr4nge [_AT_] gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
