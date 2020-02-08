48867ab07db73e5cc08e6e626617973bf9ca1679
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-08 11:24:17 +0100

    Update scan-recon.py

--- includes/scan-recon.py
+++ includes/scan-recon.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-    Copyright (C) 2016 xtr4nge [_AT_] gmail.com
+    Copyright (C) 2020 xtr4nge [_AT_] gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
